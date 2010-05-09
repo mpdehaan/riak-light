7baaed0959d05544119bc24d6b9a904be3067246
Michael DeHaan <michael@reductivelabs.com>
2010-05-09 20:03:56 +0200

    Added mode to prevent jsonification, for example, if your content is already JSON

--- lib/riak_light.rb
+++ lib/riak_light.rb
@@ -26,13 +26,14 @@ require 'json'
 
 class RiakLight
 
-  attr_accessor :server, :port, :read_quorum, :write_quorum
+  attr_accessor :server, :port, :read_quorum, :write_quorum, :jsonify
 
-  def initialize(server='127.0.0.1', port=8098, read_quorum=1, write_quorum=1)
+  def initialize(server='127.0.0.1', port=8098, jsonify=false, read_quorum=1, write_quorum=1)
      @server=server
      @port=port
      @read_quorum=read_quorum
      @write_quorum=write_quorum
+     @jsonify=jsonify
   end
 
   def conn(postfix)
@@ -53,7 +54,7 @@ class RiakLight
      # * w=2 (write quorum) how many replicas to write to before returning a successful response (default is 2).
      # * dw=0 how many replicas to commit to durable storage before returning a successful response (default is 0)
       #* returnbody=[true|false] whether to return the contents of the stored object.
-     data = JSON.dump(value)
+     data = jsonify ? JSON.dump(value) : value
      c = conn("/riak/#{bucket}/#{key}?w=#{write_quorum}")
      c.headers = headers
      c.http_post(data) # ?w=#{@write_quorum}", data)
@@ -62,13 +63,13 @@ class RiakLight
   def bucket_get(bucket, key)
      c = conn("/riak/#{bucket}/#{key}?r=#{read_quorum}")
      c.perform
-     begin 
+     #begin 
         headers = c.headers
         vclock = headers['X-Riak-Vclock']
-        return [JSON.load(c.body_str), vclock]
-     rescue
-        raise "error accessing such key '#{key}' in bucket '#{bucket}', got #{c.body_str}"
-     end
+        return [ jsonify ? JSON.load(c.body_str) : c.body_str, vclock]
+     #rescue
+     #   raise "error accessing such key '#{key}' in bucket '#{bucket}', got #{c.body_str}"
+     #end
   end
 
 end
